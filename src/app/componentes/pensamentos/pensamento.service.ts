b6d274fdd2d150a1be39d26af2bbd6186a92e1d6
Thalisson <thalissonsousa33@gmail.com>
2024-04-24 20:07:59 -0300

    feat: add favorite button

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Pensamento } from './pensamento';
 import { Observable } from 'rxjs';
 
@@ -12,8 +12,18 @@ export class PensamentoService {
 
   constructor(private http: HttpClient) { }
 
-  listar(): Observable<Pensamento[]> {
-    return this.http.get<Pensamento[]>(this.API);
+  listar(pagina: number, filtro: string): Observable<Pensamento[]> {
+    const itensPorpagina = 6;
+
+    let params = new HttpParams()
+    .set("_page", pagina)
+    .set("_limit", itensPorpagina)
+
+    if(filtro.trim().length > 2) {
+      params = params.set("q", filtro);
+    }
+
+    return this.http.get<Pensamento[]>(this.API, { params });
   }
 
   criar(pensamento: Pensamento): Observable<Pensamento> {
@@ -24,6 +34,11 @@ export class PensamentoService {
     const url = `${this.API}/${pensamento.id}`
 
     return this.http.put<Pensamento>(url, pensamento)
+  }
+
+  mudarFavorito(pensamento: Pensamento): Observable<Pensamento> {
+    pensamento.favorito = !pensamento.favorito
+    return this.editar(pensamento);
   }
 
   excluir(id: number): Observable<Pensamento> {
